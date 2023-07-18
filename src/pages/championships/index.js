84f9c4f55d01068692ef35b07f6531dd965876f9
JulianChacon11 <82472712+JulianChacon11@users.noreply.github.com>
2023-07-17 22:20:15 -0500

    Update Championships index

--- src/pages/championships/index.js
+++ src/pages/championships/index.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import ListaLigas from '../../components/lista-ligas';
+import React from "react";
+import ListaLigas from "../../components/lista-ligas";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 function Championships() {
   return (
-    <div className="flex flex-col w-full h-full items-center">
-        <div className="flex items-center justify-center w-full h-[100px] bg-[black]">
-            <h1 className="text-white text-[30px] text-center tracking-widest font-[600] uppercase"></h1>
-        </div>
+    <div className="flex flex-col w-full">
+      <Header></Header>
+      <div className="flex flex-col w-full h-full items-center">
 
-        <div className='flex items-center justify-center w-full h-[380px] bg-leagues-pattern bg-center bg-no-repeat bg-cover bg-opacity-25'>
-          <h1 className='text-white text-[50px] my-[50px] uppercase font-[800] mt-[10px] text-center'>Competiciones más famosas</h1>
+        <div className="flex items-center justify-center w-full h-[380px] bg-leagues-pattern bg-center bg-no-repeat bg-cover bg-opacity-25">
+          <h1 className="text-white text-[50px] my-[50px] uppercase font-[800] mt-[10px] text-center">
+            Competiciones más famosas
+          </h1>
         </div>
-        
 
         <div className="flex flex-col w-[90%] h-full bg-slate-200 mt-[50px] mb-[50px]">
-            <ListaLigas></ListaLigas>
+          <ListaLigas></ListaLigas>
         </div>
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
