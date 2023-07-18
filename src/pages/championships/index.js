a356038fd132a8c8a1aea4c62962448600bdec33
JulianChacon11 <82472712+JulianChacon11@users.noreply.github.com>
2023-07-17 22:05:34 -0500

    Changed championships endpoint and added searchByCountry

--- src/pages/championships/index.js
+++ src/pages/championships/index.js
@@ -9,11 +9,11 @@ function Championships() {
         </div>
 
         <div className='flex items-center justify-center w-full h-[380px] bg-leagues-pattern bg-center bg-no-repeat bg-cover bg-opacity-25'>
-          <h1 className='text-white text-[50px] my-[50px] uppercase font-[800] mt-[10px]'>Competiciones más famosas</h1>
+          <h1 className='text-white text-[50px] my-[50px] uppercase font-[800] mt-[10px] text-center'>Competiciones más famosas</h1>
         </div>
         
 
-        <div className="flex flex-col w-[90%] h-full bg-slate-200 mt-[50px]">
+        <div className="flex flex-col w-[90%] h-full bg-slate-200 mt-[50px] mb-[50px]">
             <ListaLigas></ListaLigas>
         </div>
     </div>
