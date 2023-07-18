a3e189527ccc0fd2cf9a0b56c3655a537bbdb29f
JavierTorres <torresjaqui32@gmail.com>
2023-07-18 17:03:50 -0500

    progress

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import NextProgress from "next-progress";
 export default function App({ Component, pageProps }) {
-  return    <SessionProvider session={pageProps.session}><ChakraProvider><Component {...pageProps} /></ChakraProvider></SessionProvider>
+  return    <SessionProvider session={pageProps.session}><ChakraProvider><NextProgress delay={300} options={{ showSpinner: false }} /><Component {...pageProps} /></ChakraProvider></SessionProvider>
 }
